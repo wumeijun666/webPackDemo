d71d132166f96b49ab12986e58f633f4a0c2ac63
SevenWu <seven.wu@spsoft-cn.com>
2022-03-02 09:07:15 +0800

    webpack打包css测试

---
+++ 1/webpack1-3/webpack.config.js
@@ -0,0 +1,9 @@
+var path = require('path');
+module.exports = {
+    entry: './a.js',
+    output: {
+        path: path.resolve(__dirname, ''),
+        filename: 'bundle3.js'
+    },
+    mode: 'none'
+};
