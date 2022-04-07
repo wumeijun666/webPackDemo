f3dc5bd0bf18ff7dcf494e7bc6a7d31bdeccb6b3
SevenWu <seven.wu@spsoft-cn.com>
2022-04-07 15:09:11 +0800

    提交了

--- 1/webpack1-3/webpack.config.js
+++ 1/webpack1-3/webpack.config.js
@@ -5,5 +5,11 @@ module.exports = {
         path: path.resolve(__dirname, ''),
         filename: 'bundle3.js'
     },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    },
     mode: 'none'
 };
